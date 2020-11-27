3d7e0aa4687becd17286c722ae83a94e65be7c06
Jason Alfred <jasonalfred75@gmail.com>
2020-11-26 21:39:48 -0600

    added both color and magnitude to quake data.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,46 +1,125 @@
-// Creating map object
-var myMap = L.map("map", {
-  center: [40.7, -73.95],
-  zoom: 13
-});
+function markerSize(mag) {
+  return Math.pow(mag, 7) + 15000;
+}
 
-// Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+function getColor(depth) {
+  if (depth === 0 || depth < 0) {
+    return '#F6BDC0'
+  }
+  if (depth > 0 && depth <= 5) {
+    return '#F1959B'
+  }
+  if (depth > 5 && depth <= 10) {
+    return '#F07470'
+  }
+  if (depth > 10 && depth <= 15) {
+    return '#EA4C46'
+  }
+  if (depth > 15 && depth <= 20) {
+    return '#DC1C13'
+  }
+  if (depth > 20) {
+    return '#420D09'
+  }
+}
 
-// Store API query variables
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 
-// Grab the data with d3
-d3.json(url).then(function(response) {
-  // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
-  console.log(response.features)
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-  // Loop through data
-  for (var i = 0; i < response.features.length; i++) {
- 
-    // Set the data location property to a variable
-    var location = response.features[i].geometry;
+d3
+d3.json(url).then(function (response) {
 
-    // Check for location property
-    if (location) {
+  let quakeMarkers = [];
 
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-        .bindPopup(response.features[i].descriptor));
-    }
+  for (let i = 0; i < response.features.length; i++) {
+    let features = response.features[i]
+    let datetimeCalc = new Date(features.properties.time)
 
+
+    quakeMarkers.push(
+      L.circle([features.geometry.coordinates[1], features.geometry.coordinates[0]], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "black",
+        fillColor: getColor(features.geometry.coordinates[2]),
+        radius: markerSize(features.properties.mag)
+      }).bindPopup(`<h2>${features.properties.place}</h2><hr>
+             <h3>Date: </h3>${datetimeCalc}<br>
+             <span style="color:${getColor(features.geometry.coordinates[2])};">
+             <h3>Depth: </h3>${features.geometry.coordinates[2]}
+             </span><br>
+             <h3>Magnitude: <h3>${features.properties.mag}`)
+    );
   }
 
-  // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
+  let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
 
-});
+  let darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  let satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-streets-v11",
+    accessToken: API_KEY
+  });
+
+  let outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
+
+  let litemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+
+
+
+  let earthquakes = L.layerGroup(quakeMarkers);
+
+  // Create a baseMaps object
+  let baseMaps = {
+    "Street Map": streetmap,
+    "Light Map": litemap,
+    "Dark Map": darkmap,
+    "Satellite Map": satellitemap,
+    "Outdoor Map": outdoormap,
+
+  };
+
+  // Create an overlay object
+  let overlayMaps = {
+    "Earthquakes": earthquakes
+  };
+
+  // Define a map object
+  let myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 3,
+    layers: [darkmap, earthquakes]
+  });
+
+  // Pass our map layers into our layer control
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+})
